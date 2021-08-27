0df67ec5d71716c905f6d065bc08fe70f7575c45
Gabriel <72663607+GabrielLazar@users.noreply.github.com>
2021-08-27 12:41:08 -0500

    first commit

---
+++ src/main/java/com/gabriellazar/kafkaconsumer/model/Employee.java
@@ -0,0 +1,33 @@
+package com.gabriellazar.kafkaconsumer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private LocalDate hireDate;
+    private Long jobId;
+    private BigDecimal salary;
+    private Long managerId;
+    private Long departmentId;
+}
